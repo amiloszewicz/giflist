4712fd8239b9c9778be29109c000e65c4fabc309
amiloszewicz <miloszewicz.adam@gmail.com>
2024-01-24 12:52:22 +0100

    feat component: gif list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RedditService } from '../shared/data-access/reddit.service';
+import { GifListComponent } from './ui/gif-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  template: ` <p>home works!</p> `,
+  imports: [GifListComponent],
+  template: `
+    <app-gif-list
+      [gifs]="redditService.gifs()"
+      class="grid-container"
+    ></app-gif-list>
+  `,
   styles: ``,
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  redditService = inject(RedditService);
+}
--- src/app/home/ui/gif-list.component.ts
+++ src/app/home/ui/gif-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Gif } from '../../shared/interfaces';
+import { GifPlayerComponent } from './gif-player.component';
 
 @Component({
   selector: 'app-gif-list',
   standalone: true,
-  imports: [],
-  template: ` 
-    @for (gif of gifs; track gif.permalink) {
-      <div>{{ gif.title }}</div>
+  imports: [GifPlayerComponent],
+  template: ` @for (gif of gifs; track gif.permalink) {
+    <div>
+      <app-gif-player
+        [src]="gif.src"
+        [thumbnail]="gif.thumbnail"
+      ></app-gif-player>
+    </div>
     }`,
   styles: ``,
 })
